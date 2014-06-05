387b6e764470291e1a4fbd6e3e4e4538e8034df3
Florian Garbe <f.garbe@gmx.de>
2014-06-05 11:38:32 +0200

    fixed sqlite setup, switched to ajanker sqlite setup

---
+++ ifdeftoif.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+ABSPATH=$(cd "$(dirname "$0")"; pwd)
+
+# only one single file in sqlite amalgamation version
+FNAME=sqlite3.c
+
+# set output files
+outBase="$(dirname $FNAME)/$(basename $FNAME .c)"
+outDbg="$outBase.dbg"
+outPartialPreproc="$outBase.pi"
+outErr="$outBase.err"
+outTime="$outBase.time"
+
+
+../TypeChef/ifdeftoif.sh \
+        --bdd --serializeAST --interface --debugInterface\
+        -I /usr/local/include \
+        -I /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed \
+        -I /usr/lib/gcc/x86_64-linux-gnu/4.6/include \
+        -I /usr/include/x86_64-linux-gnu \
+        -I /usr/include \
+        --platfromHeader $ABSPATH/platform.h \
+        --openFeat $ABSPATH/openfeatures.txt \
+        --featureModelFExpr $ABSPATH/fm.txt \
+        --typeSystemFeatureModelDimacs $ABSPATH/sqlite.dimacs \
+        --include $ABSPATH/partial_configuration.h \
+        --parserstatistics \
+        --writePI --ifdeftoifstatistics \
+	$FNAME \
+	#-U NDEBUG \
+
+exit

0fe20b134c041d2d761c7d8c77f5c6d859e97fb1
rhein <rhein@fim.uni-passau.de>
2014-12-12 15:14:02 +0100

    generating tests with TH3 and executing them with different load-time configs

--- simpleTestBinary.sh
+++ simpleTestBinary.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
+#For this test, sqlite3_modified.c must include the shell.c
+# #include "shell.c"
+
 #./ifdeftoif_mod.sh
 rm -f a.out
-gcc sqlite3_ifdeftoif.c 
+gcc sqlite3_ifdeftoif.c -w
 rm -f mytestdb
 
 echo "create table tbl1(one varchar(10), two smallint); insert into tbl1 values('goodbye', 20); select * from tbl1;" | ./a.out mytestdb 

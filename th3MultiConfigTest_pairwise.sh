6b87caccd0bf7b722b857bb1664b24a81bdb0fc1
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-02-04 16:03:01 +0100

    see last commit

--- th3MultiConfigTest_pairwise.sh
+++ th3MultiConfigTest_pairwise.sh
@@ -9,12 +9,13 @@ do
 	#generate test
 	cd ../TH3
 	./mkth3.tcl bugs/*.test "$th3configFile" > ../TypeChef-SQLiteIfdeftoif/th3_generated_test.c
+	./mkth3.tcl bugs/*.test "$th3configFile" > ../TypeChef-SQLiteIfdeftoif/th3_generated_test_ifdeftoif.c
 	cd ../TypeChef-SQLiteIfdeftoif
 	
 	#insert /* Alex: added initialization of our version of the azCompileOpt array */ init_azCompileOpt();
 	sed -i \
 		's/int main(int argc, char \*\*argv){/int main(int argc, char \*\*argv){\/* Alex: added initialization of our version of the azCompileOpt array *\/ init_azCompileOpt()\;/' \
-		th3_generated_test.c
+		th3_generated_test_ifdeftoif.c
 	#better never touch this sed again
 	
 	for f in ./optionstructs_ifdeftoif/pairwise/generated/id2i_optionstruct_*.h;
@@ -36,7 +37,7 @@ do
 		rm -f a.out
 		gcc -w -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_THREADSAFE=0 \
 			-include "./optionstructs_ifdeftoif/pairwise/generated/Prod$configID.h" \
-			sqlite3_ifdeftoif.c th3_generated_test.c
+			sqlite3_ifdeftoif.c th3_generated_test_ifdeftoif.c
 		#disabled all warnings! -w
 		./a.out
 		echo -e "TH3 test result: $?\n"

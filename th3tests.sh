93d9d3c7fb1752ec385a89e619ee261bc2020f5b
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-03-02 23:47:59 +0100

    echos

--- th3tests.sh
+++ th3tests.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 START_TIME=$SECONDS
 
+echo "Starting featurewise tests."
 ./th3MultiConfigTest_featurewise.sh > th3_featurewise.txt 2>&1
 
+echo "Starting pairwise tests."
 ./th3MultiConfigTest_pairwise.sh > th3_pairwise.txt 2>&1
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))

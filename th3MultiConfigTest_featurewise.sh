11dd62ce624f571e08642f75767b787dde8172fe
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-03-02 23:26:09 +0100

    testing all folders in TH3 now

--- th3MultiConfigTest_featurewise.sh
+++ th3MultiConfigTest_featurewise.sh
@@ -8,45 +8,51 @@ for th3configFile in ../TH3/cfg/*.cfg;
 do
 	#generate test
 	cd ../TH3
-	./mkth3.tcl bugs/*.test "$th3configFile" > ../TypeChef-SQLiteIfdeftoif/th3_generated_test.c
-	cd ../TypeChef-SQLiteIfdeftoif
-	
-	cp th3_generated_test.c th3_generated_test_ifdeftoif.c
-	#insert /* Alex: added initialization of our version of the azCompileOpt array */ init_azCompileOpt();
-	sed -i \
-		's/int main(int argc, char \*\*argv){/int main(int argc, char \*\*argv){\/* Alex: added initialization of our version of the azCompileOpt array *\/ init_azCompileOpt()\;/' \
-		th3_generated_test_ifdeftoif.c
-	#better never touch this sed again
-	
-	for f in ./optionstructs_ifdeftoif/feature-wise/id2i_optionstruct*.h;
+
+	for dir in */;
 	do
-		#sed filters everything but the number of the configuration
-		configID=$(basename $f | sed 's/id2i_optionstruct_//' | sed 's/.h//')
-		echo "testing #ifConfig $f on th3Config $th3configFile"
+		./mkth3.tcl $dir/*.test "$th3configFile" > ../TypeChef-SQLiteIfdeftoif/th3_generated_test.c
+		cd ../TypeChef-SQLiteIfdeftoif
+	
+		cp th3_generated_test.c th3_generated_test_ifdeftoif.c
+		#insert /* Alex: added initialization of our version of the azCompileOpt array */ init_azCompileOpt();
+		sed -i \
+			's/int main(int argc, char \*\*argv){/int main(int argc, char \*\*argv){\/* Alex: added initialization of our version of the azCompileOpt array *\/ init_azCompileOpt()\;/' \
+			th3_generated_test_ifdeftoif.c
+		#better never touch this sed again
 		
-		gcc -w -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_THREADSAFE=0 \
-			-include "./optionstructs_ifdeftoif/feature-wise/id2i_include_$configID.h" \
-			sqlite3_original.c th3_generated_test.c
-		#disabled all warnings! -w
-		./a.out
-		expectedOutputValue=$?
-		echo "TH3 non-ifdeftoif test result: $expectedOutputValue"
-		rm -f a.out
-		
-		
-		cp $f ../ifdeftoif/id2i_optionstruct.h
-		rm -f a.out
-		gcc -w -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_THREADSAFE=0 \
-			-include "./optionstructs_ifdeftoif/feature-wise/id2i_include_$configID.h" \
-			sqlite3_ifdeftoif.c th3_generated_test_ifdeftoif.c
-		#disabled all warnings! -w
-		./a.out
-		testOutputValue=$?
-		if [ $testOutputValue -eq $expectedOutputValue ] ; then
-			echo -e "Test successful\n"
-		else 
-			echo -e "TH3 test differs, ifdeftoif: $testOutputValue ; expected: $expectedOutputValue\n"
-		fi
-		rm -f a.out
+		for f in ./optionstructs_ifdeftoif/feature-wise/id2i_optionstruct*.h;
+		do
+			#sed filters everything but the number of the configuration
+			configID=$(basename $f | sed 's/id2i_optionstruct_//' | sed 's/.h//')
+			echo "testing #ifConfig $f in $dir on th3Config $th3configFile"
+			
+			# Test normal sqlite
+			gcc -w -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_THREADSAFE=0 \
+				-include "./optionstructs_ifdeftoif/feature-wise/id2i_include_$configID.h" \
+				sqlite3_original.c th3_generated_test.c
+			#disabled all warnings! -w
+			./a.out
+			expectedOutputValue=$?
+			echo "TH3 non-ifdeftoif test result: $expectedOutputValue"
+			rm -f a.out
+			
+
+			# Test ifdeftoif sqlite
+			cp $f ../ifdeftoif/id2i_optionstruct.h
+			gcc -w -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_THREADSAFE=0 \
+				-include "./optionstructs_ifdeftoif/feature-wise/id2i_include_$configID.h" \
+				sqlite3_ifdeftoif.c th3_generated_test_ifdeftoif.c
+			#disabled all warnings! -w
+			./a.out
+			testOutputValue=$?
+			echo "TH3 ifdeftoif test result: $testOutputValue"
+			if [ $testOutputValue -eq $expectedOutputValue ] ; then
+				echo -e "Test successful\n"
+			else 
+				echo -e "TH3 test differs, ifdeftoif: $testOutputValue ; expected: $expectedOutputValue\n"
+			fi
+			rm -f a.out
+		done
 	done
 done

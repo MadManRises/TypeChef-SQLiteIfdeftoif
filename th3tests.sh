fc27b5711b8cbcac2c661cb1d81326e3116035d6
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-03-05 23:37:43 +0100

    echo string changes, time information added

--- th3tests.sh
+++ th3tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 START_TIME=$SECONDS
 
-echo "Starting featurewise tests."
+echo "Starting featurewise tests at $(date +"%T")."
 ./th3MultiConfigTest_featurewise.sh > th3_featurewise.txt 2>&1
 
-echo "Starting pairwise tests."
+echo "Starting pairwise tests at $(date +"%T")."
 ./th3MultiConfigTest_pairwise.sh > th3_pairwise.txt 2>&1
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))

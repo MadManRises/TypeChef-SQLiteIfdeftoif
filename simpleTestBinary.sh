c4be8338760619dcf16fc9841dbc0e3f7aada92f
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-03-30 23:24:05 +0200

    changed simple test binary script

--- simpleTestBinary.sh
+++ simpleTestBinary.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-#For this test, sqlite3_modified.c must include the shell.c
-# #include "shell.c"
-
-#./ifdeftoif_mod.sh
+./ifdeftoif_mod.sh
 rm -f a.out
 gcc shell.c sqlite3_ifdeftoif.c -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_THREADSAFE=0 -w
 rm -f mytestdb
 
-echo "create table tbl1(one varchar(10), two smallint); insert into tbl1 values('goodbye', 20); select * from tbl1;" | ./a.out mytestdb 
+result=$(echo "create table tbl1(one varchar(10), two smallint); insert into tbl1 values('goodbye', 20); select * from tbl1;" | ./a.out mytestdb)
+if [ "$result" == "goodbye|20" ]; then
+    echo "Test successful"
+else
+    echo "Test failed, expected 'goodbye|20' but got $result"
+fi
 
 rm -f mytestdb
 rm -f a.out
 
+exit

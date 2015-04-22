c1e06452ac547dc7551c10ad0040a1cf4fbbddcf
rhein <rhein@fim.uni-passau.de>
2015-04-22 16:53:05 +0200

    SPLCATool

--- optionstructs_ifdeftoif/pairwise/generatePairwiseConfigs_SPLCATool.sh
+++ optionstructs_ifdeftoif/pairwise/generatePairwiseConfigs_SPLCATool.sh
@@ -3,7 +3,7 @@
 # make sure that limitations in ../ifdeftoifHelpers/custom_limitations.txt
 # are reflected in moreConstraints.dimacs
 
-java -Xmx5500m -jar /local/SPLCATool_Workspace/SPLCATool.jar \
+java -Xmx5500m -jar ../SPLCATool.jar \
 -t t_wise -a ICPL -fm ../../sqlite.dimacs -s 2 \
 -focusVariables focusVariables.txt \
 -o generatedConfigs.ca2.csv \

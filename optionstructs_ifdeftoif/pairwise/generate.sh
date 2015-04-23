2a9463a45e1dbe03d57f5a1296047f4532bff610
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-04-23 19:37:47 +0200

    fixed SQLITE_OS_UNIX #defines

--- optionstructs_ifdeftoif/pairwise/generate.sh
+++ optionstructs_ifdeftoif/pairwise/generate.sh
@@ -1,3 +1,5 @@
 ./generatePairwiseConfigs_SPLCATool.sh
 python generateHeaders.py
 ./cppHeadersToId2iConfigs.sh
+#replace SQLITE_OS_UNIX with SQLITE_OS_UNIX 1
+find -name Prod*.h | sed -i 's/#define SQLITE_OS_UNIX/#define SQLITE_OS_UNIX 1/g'

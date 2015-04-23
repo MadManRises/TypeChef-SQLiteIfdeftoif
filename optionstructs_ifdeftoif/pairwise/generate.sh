5560d5824d63cfd5d20bc1a67b872ee2162254d1
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-04-23 19:54:08 +0200

    removed SQLITE_OMIT_VIEW from focus variables; generate.sh script now empties the /generated folder before generating

--- optionstructs_ifdeftoif/pairwise/generate.sh
+++ optionstructs_ifdeftoif/pairwise/generate.sh
@@ -1,5 +1,7 @@
+rm -r generated
+mkdir generated
 ./generatePairwiseConfigs_SPLCATool.sh
 python generateHeaders.py
 ./cppHeadersToId2iConfigs.sh
 #replace SQLITE_OS_UNIX with SQLITE_OS_UNIX 1
-find -name Prod*.h | sed -i 's/#define SQLITE_OS_UNIX/#define SQLITE_OS_UNIX 1/g'
+find -name Prod*.h | xargs sed -i 's/#define SQLITE_OS_UNIX/#define SQLITE_OS_UNIX 1/g'

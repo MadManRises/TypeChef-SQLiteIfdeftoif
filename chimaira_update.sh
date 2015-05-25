c40f44f02f1f95a67790e53be7407cbb4c8c710a
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-05-25 20:19:54 +0200

    fixed typo

--- chimaira_update.sh
+++ chimaira_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `seq -w 1 17`: do
+for i in `seq -w 1 17`; do
     sbatch -A spl -p chimaira --time=00:05:00 -o ~/slurm/logfile_update-%j.txt -w chimaira${i} update_chimaira.sh
 done
 

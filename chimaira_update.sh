38be62cf61c5f380a145742e39dc52cc58fd3394
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-05-25 20:15:01 +0200

    removed mail notification

--- chimaira_update.sh
+++ chimaira_update.sh
@@ -4,4 +4,4 @@ for i in `seq -w 1 17`: do
     sbatch -A spl -p chimaira --time=00:05:00 -o ~/slurm/logfile_update-%j.txt -w chimaira${i} update_chimaira.sh
 done
 
-echo "Stop slacking off." | mail -s "Chimaira update finished." fgarbe@fim.uni-passau.de
+# echo "Stop slacking off." | mail -s "Chimaira update finished." fgarbe@fim.uni-passau.de

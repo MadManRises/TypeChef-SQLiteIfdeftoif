edeb6335d9802c3bb14d09ebdebc44abf5dff24b
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-05-28 00:09:31 +0200

    directory fix

--- chimaira_update.sh
+++ chimaira_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in `seq -w 1 17`; do
-    sbatch -A spl -p chimaira --time=00:05:00 -o ~/slurm/logfile_update-%j.txt -w chimaira${i} update_chimaira.sh
+    sbatch -A spl -p chimaira --time=00:05:00 -o /home/garbe/chimaira/update-%j.txt -w chimaira${i} update_chimaira.sh
 done
 
 # echo "Stop slacking off." | mail -s "Chimaira update finished." fgarbe@fim.uni-passau.de

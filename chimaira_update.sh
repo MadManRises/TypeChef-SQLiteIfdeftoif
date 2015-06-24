3591a72edbdeb9804e6510c5822f7f182ee3f472
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-06-24 15:41:15 +0200

    chimaira update script extended, now also installing typechef and hercules

--- chimaira_update.sh
+++ chimaira_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in `seq -w 1 17`; do
-    sbatch -A spl --mem=500 --cpus-per-task=1 -p chimaira --time=00:01:00 -o /home/garbe/chimaira/update-${i}.txt -w chimaira${i} update_chimaira.sh
+    sbatch -A spl --cpus-per-task=1 -p chimaira --time=00:05:00 -o /home/garbe/chimaira/update-${i}.txt -w chimaira${i} update_chimaira.sh
 done
 
 # echo "Stop slacking off." | mail -s "Chimaira update finished." fgarbe@fim.uni-passau.de
--- update_chimaira.sh
+++ update_chimaira.sh
@@ -13,13 +13,42 @@ echo =================================================================
 cd $localDir
 
 # Initialize
+
+if [ ! -d TH3 ]; then
+    # get TH3
+    cp -r ~/TH3 .
+fi
+
+if [ ! -d TypeChef ]; then
+    # get TypeChef
+    git clone https://github.com/aJanker/TypeChef.git
+    ./TypeChef/publish.sh
+else
+    # update TypeChef
+    cd TypeChef
+    if [ "$(git pull)" != "Already up-to-date." ]; then
+    ./publish.sh
+    fi
+    cd $OLDPWD
+fi
+
+if [ ! -d Hercules ]; then
+    # get Hercules
+    git clone https://github.com/joliebig/Hercules.git
+    ./Hercules/mkrun.sh
+else
+    # update Hercules
+    cd Hercules
+    if [ "$(git pull)" != "Already up-to-date." ]; then
+    ./mkrun.sh
+    fi
+    cd $OLDPWD
+fi
+
 if [ ! -d TypeChef-SQLiteIfdeftoif ]; then
     # get SQLITE
     git clone https://github.com/fgarbe/TypeChef-SQLiteIfdeftoif
-
-    # get TH3
-    cp -r ~/TH3 .
 else
     # update SQLITE
-    cd TypeChef-SQLiteIfdeftoif/ && git pull && cd -
+    cd TypeChef-SQLiteIfdeftoif/ && git pull && cd $OLDPWD
 fi

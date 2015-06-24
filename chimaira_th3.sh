cce6ce3370de09d988dc2ae1c4fa1fa803c52e16
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-06-24 21:43:26 +0200

    added th3 script

---
+++ chimaira_th3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH -o /home/garbe/chimaira/featurewise-%a.txt
+#SBATCH --job-name=hercules-sqlite
+#SBATCH -p chimaira
+#SBATCH -A spl
+#SBATCH --get-user-env
+#SBATCH --ntasks 1
+#SBATCH --array=0-233
+
+#SBATCH --time=00:30:00 # 30 minutes max
+
+#SBATCH --cpus-per-task 1   # 1 for easier apps experiment
+
+##SBATCH --cpus-per-task 10     #set to 10 for full chimaira core per apk (60 GB ram)
+##SBATCH --exclusive        #remove for easier apps experiment
+
+# 6552 different test scenarios
+
+taskName="hercules-sqlite-th3"
+localDir=/local/garbe
+resultDir=~/sqlite
+lastJobNo=233
+
+# Call this script as follows:
+# sbatch slurm_featurewise.sh
+
+echo =================================================================
+echo % HERCULES TH3 GENERATION\(s\)
+echo % Task ID: ${SLURM_ARRAY_TASK_ID}
+echo % JOB ID: ${SLURM_JOBID}
+echo =================================================================
+
+cd $localDir
+
+cd TypeChef-SQLiteIfdeftoif
+./parallel_th3_test_ifdeftoif.sh ${SLURM_ARRAY_TASK_ID}
+
+# send mail notification for last job
+if [ ${SLURM_ARRAY_TASK_ID} -eq $lastJobNo ]; then
+    echo "Stop slacking off." | mail -s "Chimaira TH3 job finished." fgarbe@fim.uni-passau.de
+fi

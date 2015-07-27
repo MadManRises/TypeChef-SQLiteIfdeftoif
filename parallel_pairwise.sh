f2e74030d60d91ed775288d7b8266f122c07a294
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-07-27 13:07:51 +0200

    fix: wrong sqlite file was used for compilation

--- parallel_pairwise.sh
+++ parallel_pairwise.sh
@@ -49,7 +49,7 @@ if [ $1 -lt $TOTAL ]; then
         # Test normal sqlite
         originalGCC=$(gcc -w -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_THREADSAFE=0 \
             -include "../TypeChef-SQLiteIfdeftoif/optionstructs_ifdeftoif/pairwise/generated/Prod$configID.h" \
-            ../TypeChef-SQLiteIfdeftoif/sqlite3_original_fixed.c th3_generated_test.c 2>&1)
+            ../TypeChef-SQLiteIfdeftoif/sqlite3_original.c th3_generated_test.c 2>&1)
         # If gcc returns errors skip the testing
         if [ $? != 0 ]
         then

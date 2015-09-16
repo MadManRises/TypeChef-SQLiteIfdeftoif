4edef9f55354596cd93f24008c556c53ae115a42
Florian Garbe <fgarbe@fim.uni-passau.de>
2015-09-16 16:59:44 +0200

    updated update script

--- update_chimaira.sh
+++ update_chimaira.sh
@@ -5,50 +5,56 @@ localDir=/local/garbe
 
 # Call this script as follows:
 # chimaira_update.sh
+if [ -d $localDir ]; then
+    echo =================================================================
+    echo % HERCULES SQLITE UPDATE\(s\)
+    echo =================================================================
 
-echo =================================================================
-echo % HERCULES SQLITE UPDATE\(s\)
-echo =================================================================
+    cd $localDir
 
-cd $localDir
+    # Initialize
 
-# Initialize
 
-if [ ! -d TH3 ]; then
     # get TH3
+    rm -rf TH3/
     cp -r ~/TH3 .
-fi
 
-if [ ! -d TypeChef ]; then
-    # get TypeChef
-    git clone https://github.com/aJanker/TypeChef.git
-    ./TypeChef/publish.sh
-else
-    # update TypeChef
-    cd TypeChef
-    if [ "$(git pull)" != "Already up-to-date." ]; then
-    ./publish.sh
+    if [ ! -d TypeChef ]; then
+        # get TypeChef
+        git clone https://github.com/aJanker/TypeChef.git
+        ./TypeChef/publish.sh
+    else
+        # update TypeChef
+        cd TypeChef
+	    pull=$(git pull 2>&1)
+        if [ $pull != "Already up-to-date." ] && [ $pull != *"fatal:"* ]; then
+            ./publish.sh
+        else
+            echo "Skipping publish."
+        fi
+        cd $OLDPWD
     fi
-    cd $OLDPWD
-fi
 
-if [ ! -d Hercules ]; then
-    # get Hercules
-    git clone https://github.com/joliebig/Hercules.git
-    ./Hercules/mkrun.sh
-else
-    # update Hercules
-    cd Hercules
-    if [ "$(git pull)" != "Already up-to-date." ]; then
-    ./mkrun.sh
+    if [ ! -d Hercules ]; then
+        # get Hercules
+        git clone https://github.com/joliebig/Hercules.git
+        ./Hercules/mkrun.sh
+    else
+        # update Hercules
+        cd Hercules
+        if [ "$(git pull)" != "Already up-to-date." ]; then
+        ./mkrun.sh
+        fi
+        cd $OLDPWD
     fi
-    cd $OLDPWD
-fi
 
-if [ ! -d TypeChef-SQLiteIfdeftoif ]; then
-    # get SQLITE
-    git clone https://github.com/fgarbe/TypeChef-SQLiteIfdeftoif
+    if [ ! -d TypeChef-SQLiteIfdeftoif ]; then
+        # get SQLITE
+        git clone https://github.com/fgarbe/TypeChef-SQLiteIfdeftoif
+    else
+        # update SQLITE
+        cd TypeChef-SQLiteIfdeftoif/ && git pull && cd $OLDPWD
+    fi
 else
-    # update SQLITE
-    cd TypeChef-SQLiteIfdeftoif/ && git pull && cd $OLDPWD
+    echo "Wrong machine? Script cancelled"
 fi

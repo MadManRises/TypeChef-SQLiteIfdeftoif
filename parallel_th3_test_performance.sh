9b5b8872c8adf91dac4d60f07572cdf6a2d63465
Florian Garbe <fgarbe@fim.uni-passau.de>
2016-01-20 13:57:24 +0100

    added scripts for performance evaluation

---
+++ parallel_th3_test_performance.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+homeDir=/home/garbe
+localDir=/local/garbe
+sqliteDir=$localDir/TypeChef-SQLiteIfdeftoif
+herculesDir=$localDir/Hercules
+
+if [ $USER == "flo" ]; then
+    homeDir=/home/flo # th3_generated_performance here; contains the generated SQLite ifdeftoif files
+    localDir=/home/flo/TypeChef	# TH3 here
+    sqliteDir=$localDir/TypeChef-SQLiteIfdeftoif 
+    herculesDir=$localDir/Hercules
+fi
+if [ $USER == "rhein" ]; then
+    homeDir=/local/ifdeftoif/ # th3_generated_performance here; contains the generated SQLite ifdeftoif files
+    localDir=/local/ifdeftoif/	# TH3 here
+    sqliteDir=/local/ifdeftoif/TypeChef-SQLiteIfdeftoif
+    herculesDir=/local/ifdeftoif/Hercules
+fi
+
+TESTDIRS=$(find $localDir/TH3 -name '*test' ! -path "$localDir/TH3/stress/*" -printf '%h\n' | sort -u | wc -l)
+CFGFILES=$(find $localDir/TH3/cfg/ -name "*.cfg" ! -name "cG.cfg" | wc -l)
+TOTAL=$(( $TESTDIRS * $CFGFILES ))
+
+TESTDIRNO=$(( ($1 / $CFGFILES) + 1 ))
+TH3CFGNO=$(( ($1 % $CFGFILES) + 1 ))
+
+if [ $1 -lt $TOTAL ]; then
+    cd $homeDir
+    mkdir th3_generated_performance 2>/dev/null
+    cd th3_generated_performance
+    rm -rf tmp_$1
+    mkdir tmp_$1
+    cd tmp_$1
+    workingDir=$homeDir/th3_generated_performance/tmp_$1
+
+    # find $1'th sub directory containing .test files, excluding stress folder
+    TESTDIR=$(find $localDir/TH3 -name '*test' ! -path "./TH3/stress/*" -printf '%h\n' | sort -u | head -n $TESTDIRNO | tail -n 1)
+    TESTDIRBASE=$(basename $TESTDIR)
+
+    # find $3'th .cfg
+    TH3CFG=$(find $localDir/TH3/cfg/ -name "*.cfg" ! -name "cG.cfg" | sort | head -n $TH3CFGNO | tail -n 1)
+    TH3CFGBASE=$(basename $TH3CFG)
+
+    echo "Generating ifdeftoif test file for testdir #$TESTDIRNO $TESTDIRBASE and th3 config #$TH3CFGNO $TH3CFGBASE"
+    cd $localDir/TH3
+    # Ignore ctime03.test since it features a very large struct loaded with 100 different #ifdefs & #elses
+    # Ignore date2.test since it returns the systems local time; this makes string differences in test results impossible
+    TESTFILES=$(find $TESTDIR -name "*.test" ! -name "ctime03.test" ! -name "date2.test" | sort)
+    ./mkth3.tcl $TESTFILES "$TH3CFG" > $workingDir/th3_generated_test.c
+    cd $workingDir
+
+    # Copy files used for compilation into temporary directory
+    cp $sqliteDir/id2i_optionstruct.h .
+    cp $sqliteDir/sqlite3_modified.c .
+    cp $sqliteDir/sqlite3.h .
+    cp $sqliteDir/ifdeftoif_helpers/custom_limitations.txt sqlite3_modified.pc
+    echo -e "\n" >> sqlite3_modified.c
+    cat th3_generated_test.c >> sqlite3_modified.c
+
+    #insert /* Alex: added initialization of our version of the azCompileOpt array */ init_azCompileOpt();
+    sed -i 's/int main(int argc, char \*\*argv){/int main(int argc, char \*\*argv){\/* Alex: added initialization of our version of the azCompileOpt array *\/\n  init_azCompileOpt()\;/' \
+        sqlite3_modified.c
+    #better never touch this sed again
+
+    # start ifdeftoif
+    cd $herculesDir
+    ./ifdeftoif.sh  \
+        --bdd --interface --debugInterface\
+        -I /usr/local/include \
+        -I /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed \
+        -I /usr/lib/gcc/x86_64-linux-gnu/4.8/include \
+        -I /usr/include/x86_64-linux-gnu \
+        -I /usr/include \
+        --platfromHeader $sqliteDir/platform.h \
+        --openFeat $sqliteDir/openfeatures.txt \
+        --featureModelFExpr $sqliteDir/fm.txt \
+        --typeSystemFeatureModelDimacs $sqliteDir/sqlite.dimacs \
+        --include $sqliteDir/partial_configuration.h \
+        --performance --simpleSwitch \
+        -U WIN32 -U _WIN32 \
+        -U __CYGWIN__ -U __MINGW32__ \
+        $workingDir/sqlite3_modified.c > $workingDir/../log_$1.txt 2>&1
+    cd $workingDir
+
+    # Change optionstruct path in the first line of the transformed file
+    sed -i 's/#include ".*id2i_optionstruct\.h"/#include "id2i_optionstruct.h"/' sqlite3_modified_performance.c
+    mv sqlite3_modified_performance.c ../sqlite3_performance_$1.c
+    cd ..
+    #rm -rf tmp_$1
+fi
---
+++ performance.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+th3IfdeftoifDir=/home/$USER/th3_generated_performance
+resultDir=~/sqlite
+jobExportDir=$resultDir/pf_$1
+if [ $USER == "rhein" ]; then
+    th3IfdeftoifDir=/home/garbe/th3_generated_performance
+fi
+
+TESTDIRS=$(find ../TH3 -name '*test' ! -path "*/TH3/stress/*" -printf '%h\n' | sort -u | wc -l)
+CFGFILES=$(find ../TH3/cfg/ -name "*.cfg" ! -name "cG.cfg" | wc -l)
+#IFCONFIGS=$(find ../TypeChef-SQLiteIfdeftoif/optionstructs_ifdeftoif/feature-wise/ -name "id2i_optionstruct_*.h" | wc -l)
+TOTAL=$(( $TESTDIRS * $CFGFILES ))
+
+TESTDIRNO=$(( ($1 / $CFGFILES) + 1 ))
+TH3CFGNO=$(( ($1 % $CFGFILES)  + 1 ))
+TH3IFDEFNO=$1
+
+if [ $1 -lt $TOTAL ]; then
+    cd ..
+    rm -rf tmppf_$1
+    mkdir tmppf_$1
+    cd tmppf_$1
+
+    # find $1'th sub directory containing .test files, excluding stress folder
+    TESTDIR=$(find ../TH3 -name '*test' ! -path "*/TH3/stress/*" -printf '%h\n' | sort -u | head -n $TESTDIRNO | tail -n 1)
+    TESTDIRBASE=$(basename $TESTDIR)
+
+    # find $2'th optionstruct
+    IFCONFIG=../TypeChef-SQLiteIfdeftoif/optionstructs_ifdeftoif/performance/allyes_optionstruct.h
+    IFCONFIGBASE=$(basename $IFCONFIG)
+
+    # find $3'th .cfg
+    TH3CFG=$(find ../TH3/cfg/ -name "*.cfg" ! -name "cG.cfg" | sort | head -n $TH3CFGNO | tail -n 1)
+    TH3CFGBASE=$(basename $TH3CFG)
+
+    # count .test files
+    TESTFILENO=$(find $TESTDIR -name "*.test" | wc -l)
+
+    cd ../TH3
+    # Ignore ctime03.test since it features a very large struct loaded with 100 different #ifdefs & #elses
+    # Ignore date2.test since it returns the systems local time; this makes string differences in test results impossible
+    TESTFILES=$(find $TESTDIR -name "*.test" ! -name "ctime03.test" ! -name "date2.test" | sort)
+    ./mkth3.tcl $TESTFILES "$TH3CFG" > ../tmppf_$1/th3_generated_test.c
+    cd ../tmppf_$1
+
+    #sed filters everything but the number of the configuration
+    configID=$(basename $IFCONFIG | sed 's/id2i_optionstruct_//' | sed 's/.h//')
+
+    # Copy files used for compilation into temporary directory
+    cp $IFCONFIG id2i_optionstruct.h
+    cp ../TypeChef-SQLiteIfdeftoif/sqlite3.h sqlite3.h
+
+    if ! cp $th3IfdeftoifDir/sqlite3_performance_$TH3IFDEFNO.c sqlite3_performance.c; then
+        ./../TypeChef-SQLiteIfdeftoif/parallel_th3_test_performance.sh $1
+    fi
+    if cp $th3IfdeftoifDir/sqlite3_performance_$TH3IFDEFNO.c sqlite3_performance.c; then
+        echo "performance testing: jobid $1 ifdeftoif $TH3IFDEFNO; #ifConfig $IFCONFIGBASE on $TESTFILENO .test files in $TESTDIRBASE with th3Config $TH3CFGBASE at $(date +"%T")"
+
+        # Compile normal sqlite
+        originalGCC=$(gcc -w -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_THREADSAFE=0 \
+            -I /usr/local/include \
+            -I /usr/lib/gcc/x86_64-linux-gnu/4.8/include-fixed \
+            -I /usr/lib/gcc/x86_64-linux-gnu/4.8/include \
+            -I /usr/include/x86_64-linux-gnu \
+            -I /usr/include \
+            -include "../TypeChef-SQLiteIfdeftoif/optionstructs_ifdeftoif/performance/allyes_include.h" \
+            -include "../TypeChef-SQLiteIfdeftoif/partial_configuration.h" \
+            -include "../TypeChef-SQLiteIfdeftoif/sqlite3_defines.h" \
+            ../TypeChef-SQLiteIfdeftoif/sqlite3_original.c th3_generated_test.c 2>&1)
+
+        if [ $? != 0 ]
+            then
+                echo -e $originalGCC
+                echo -e "\n\ncan not compile original gcc file"
+            else
+                # Run normal binary
+                ./a.out
+                # Clear temporary variant files
+                rm -rf *.out
+                rm -rf *.db
+                rm -rf *.lock
+
+                performanceGCC=$(gcc -w -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_THREADSAFE=0 sqlite3_performance.c 2>&1)
+                # gcc returns errors
+                if [ $? != 0 ]; then
+                    echo "can not compile performance file"
+                else
+                    # Run ifdeftoif binary
+                    ./a.out
+                fi
+        fi
+        # Clear temporary simulator files
+        rm -rf *.db
+        rm -rf *.out
+        rm -rf *.lock
+    fi
+    cd ..
+    #rm -rf tmppf_$1
+fi

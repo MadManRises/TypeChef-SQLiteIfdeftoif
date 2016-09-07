e60004094728dbe6b9fc248ee287ebbd749139a4
Florian Garbe <fgarbe@fim.uni-passau.de>
2016-09-07 10:51:07 +0200

    added random cfg generation files

---
+++ optionstructs_ifdeftoif/random/cppHeadersToId2iConfigs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Alter the given .h file to match the format of a .config file
+mkdir -p generated/tmp
+for f in ./generated/id2i_include*.h;
+do
+    sed 's/#define \([A-Z_0-9]*\)$/\1=y/g' $f > generated/tmp/$(basename $f .h).config
+done
+
+#for f in ./generated/id2i_include*.h;
+#do
+#    comm -3 ../alwayson.txt <(sed 's/#define \([A-Z_0-9]*\)$/\1/g' $f) > generated/$(basename $f .h).info
+#done
+
+cd ../../
+
+# Move list of all SQLite and TH3 test features into ifdeftoif folder
+mv ../ifdeftoif/featureSet.txt ../ifdeftoif/featureSetTMP.txt
+cp featureSet.txt ../ifdeftoif
+
+for c in ./optionstructs_ifdeftoif/random/generated/tmp/id2i_include*.config;
+do
+     # Start Hercules; only struct generation with the .config files
+    ./../Hercules/ifdeftoif.sh --featureConfig $c
+    configID=$(basename $c | sed 's/id2i_include_//' | sed 's/.config//')
+    mv ../ifdeftoif/id2i_optionstruct.h optionstructs_ifdeftoif/random/generated/id2i_optionstruct_$configID.h
+    rm $c
+done
+rm -rf ../ifdeftoif/id2i_optionstruct.h optionstructs_ifdeftoif/random/generated/tmp
+
+# Move feature list back
+mv ../ifdeftoif/featureSetTMP.txt ../ifdeftoif/featureSet.txt

1a037bc366c2b87306daa4e19fe9969726642783
Florian Garbe <fgarbe@fim.uni-passau.de>
2016-09-19 12:13:50 +0200

    moved chimaira scripts to a new folder

---
+++ chimaira_scripts/chimaira_ifdeftoif.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH -o /home/garbe/logs/th3-%a.txt
+#SBATCH --job-name=sqlite-th3
+#SBATCH -p chimaira
+#SBATCH -A spl
+#SBATCH --get-user-env
+#SBATCH --ntasks 1
+#SBATCH --mem=13000
+#SBATCH --array=0-311
+
+#SBATCH --time=01:30:00 # 1h30m max
+
+#SBATCH --cpus-per-task 1   # 1 for easier apps experiment
+
+##SBATCH --cpus-per-task 10     #set to 10 for full chimaira core per apk (60 GB ram)
+##SBATCH --exclusive        #remove for easier apps experiment
+
+# 26 test cfgs; 12 test folders
+# 26*12 = 312 different test scenarios
+
+taskName="hercules-sqlite-ifdeftoif"
+localDir=/local/garbe
+resultDir=~/sqlite
+lastJobNo=233
+
+# Call this script as follows:
+# sbatch slurm_featurewise.sh
+
+echo =================================================================
+echo % HERCULES TH3 GENERATION\(s\)
+echo % Task ID: ${SLURM_ARRAY_TASK_ID}
+echo % JOB ID: ${SLURM_JOBID}
+echo =================================================================
+
+cd $localDir
+
+cd TypeChef-SQLiteIfdeftoif
+./parallel_th3_test_ifdeftoif.sh ${SLURM_ARRAY_TASK_ID}
+
+# send mail notification for last job
+if [ ${SLURM_ARRAY_TASK_ID} -eq $lastJobNo ]; then
+    echo "Stop slacking off." | mail -s "Chimaira TH3 job finished." fgarbe@fim.uni-passau.de
+fi

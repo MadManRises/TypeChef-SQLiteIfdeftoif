58c291772e6b273487efbbf6426d6835f7718c62
schuetzo <schuetzo-martin@web.de>
2018-01-31 16:43:35 +0100

    copy so

--- performance_allyes.sh
+++ performance_allyes.sh
@@ -73,6 +73,7 @@ if [ $1 -lt $TOTAL ]; then
     sed -i 's/return nFail\;/id2iperf_time_end()\;\n  return nFail\;/' th3_generated_test.c
 
     cp ../TypeChef-SQLiteIfdeftoif/sqlite3.h sqlite3.h
+    cp /local/schuetz/PerfInst/build/libPerfInst.so libPerfInst.so
 
 
     # Test allyes variant
@@ -107,7 +108,7 @@ if [ $1 -lt $TOTAL ]; then
     if cp $th3IfdeftoifDir/sqlite3_performance_$TH3IFDEFNO.c sqlite3_performance.c; then
         echo "performance testing allyes performance: jobid $1 ifdeftoif $TH3IFDEFNO on $TESTFILENO .test files in $TESTDIRBASE with th3Config $TH3CFGBASE at $(date +"%T")"
 
-        performanceGCC=$($GCC -w -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_THREADSAFE=0 sqlite3_performance.c /local/schuetz/PerfInst/build/libPerfInst.so)
+        performanceGCC=$($GCC -w -DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_THREADSAFE=0 sqlite3_performance.c libPerfInst.so)
         # gcc returns errors
         if [ $? != 0 ]; then
             echo "can not compile allyes performance"

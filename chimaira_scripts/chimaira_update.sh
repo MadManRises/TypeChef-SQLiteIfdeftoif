86e90284e6b693a2e0ace8faac70b4234d74be23
schuetzo <schuetzo-martin@web.de>
2018-03-26 14:27:28 +0200

    Update

--- chimaira_scripts/chimaira_update.sh
+++ chimaira_scripts/chimaira_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in `seq -s " " -f %02g 1 17`; do
-    sbatch -A anywhere --cpus-per-task=1 --mem=4G -p anywhere --time=00:40:00 -o /scratch/schuetz/logs/update-${i}.txt -w chimaira${i} /scratch/schuetz/update_chimaira.sh
+    sbatch -A anywhere --cpus-per-task=1 --mem=4G -p anywhere --time=00:40:00 -o /scratch/schuetz/logs/update-${i}.txt -w chimaira${i} update_chimaira.sh
 done
 
 # echo "Stop slacking off." | mail -s "Chimaira update finished." schuetzo-martin@web.de
--- update_chimaira.sh
+++ update_chimaira.sh
@@ -17,69 +17,79 @@ if [ -d $localDir ]; then
 
     # get TH3
     rm -rf TH3/
-    cp -r /scratch/TH3 .
+    cp -r /scratch/schuetz/TH3 .
 
-    if [ ! -d TypeChef ]; then
+    cp -rf /scratch/schuetz/TypeChef .
+
+    #if [ ! -d TypeChef ]; then
         # get TypeChef
-        git clone https://github.com/aJanker/TypeChef.git
+        
+    #    git clone https://github.com/aJanker/TypeChef.git
         cd TypeChef
         java -jar ./sbt-launch.jar update compile
         cd $OLDPWD
-    else
+    #else
         # update TypeChef
-        cd TypeChef
-	    pull=$(git pull 2>&1)
-        if [ $pull != "Already up-to-date." ] && [ $pull != *"fatal:"* ]; then
-            java -jar ./sbt-launch.jar update compile
-        else
-            echo "Skipping TypeChef ./publish.sh"
-        fi
-        cd $OLDPWD
-    fi
+    #    cd TypeChef
+	  #  pull=$(git pull 2>&1)
+    #    if [ $pull != "Already up-to-date." ] && [ $pull != *"fatal:"* ]; then
+    #        java -jar ./sbt-launch.jar update compile
+    #    else
+    #        echo "Skipping TypeChef ./publish.sh"
+    #    fi
+    #    cd $OLDPWD
+    #fi
 
-    if [ ! -d Hercules ]; then
+    cp -rf /scratch/schuetz/Hercules .
+
+    #if [ ! -d Hercules ]; then
         # get Hercules
-        git clone https://github.com/MadManRises/Hercules.git
-        ./Hercules/mkrun.sh
-    else
-        # update Hercules
+    #    git clone https://github.com/MadManRises/Hercules.git
+    #    ./Hercules/mkrun.sh
+    #else
+    #    # update Hercules
         cd Hercules
-        pull=$(git pull 2>&1)
-        if [ "$pull" != "Already up-to-date." ] && [ "$pull" != *"fatal:"* ]; then
+    #    pull=$(git pull 2>&1)
+    #    if [ "$pull" != "Already up-to-date." ] && [ "$pull" != *"fatal:"* ]; then
             ./mkrun.sh
-        else
-            echo "Skipping Hercules ./mkrun.sh"
-        fi
+    #    else
+    #        echo "Skipping Hercules ./mkrun.sh"
+    #    fi
         cd $OLDPWD
-    fi
-
-    if [ ! -d TypeChef-SQLiteIfdeftoif ]; then
-        # get SQLITE
-        git clone https://github.com/MadManRises/TypeChef-SQLiteIfdeftoif.git
-    else
-        # update SQLITE
-        cd TypeChef-SQLiteIfdeftoif/ && git pull && cd $OLDPWD
-    fi
+    #fi
     
-    if [ ! -d PerfInst ]; then
+    cp -rf /scratch/schuetz/TypeChef-SQLiteIfdeftoif .
+    
+
+    #if [ ! -d TypeChef-SQLiteIfdeftoif ]; then
+        # get SQLITE
+    #    git clone https://github.com/MadManRises/TypeChef-SQLiteIfdeftoif.git
+    #else
+        # update SQLITE
+    #    cd TypeChef-SQLiteIfdeftoif/ && git pull && cd $OLDPWD
+    #fi
+    
+    cp -rf /scratch/schuetz/PerfInst .
+    
+    #if [ ! -d PerfInst ]; then
         # get PerfInst
-        git clone https://github.com/vulder/PerfInst.git
+        #git clone https://github.com/vulder/PerfInst.git
         cd PerfInst
-        mkdir build
+        mkdir -p build
         cd build
         cmake ..
         make
         cd $localDir
-    else
+    #else
         # update PerfInst
-        cd PerfInst
-        pull=$(git pull 2>&1)
+    #    cd PerfInst
+    #    pull=$(git pull 2>&1)
         
-        cd build
-        make
+    #    cd build
+    #    make
         
-        cd $localDir
-    fi
+    #    cd $localDir
+    #fi
     
 else
     echo "Wrong machine? Script cancelled"
